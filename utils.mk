a845d79be92218ee06f826daa6e06262f72d95ac
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 05:16:33 +0300

    utils.mk: add urlcodes target and info messages

--- utils.mk
+++ utils.mk
@@ -1,4 +1,13 @@
+urlcodes:
+	@echo "=======> Check URLs for response code"
+	@grep -Eiho "https?://[^\"\\'> ]+" *.*      \
+		| grep -v fileserver.intranet       \
+		| xargs -P10 -I{} curl -o /dev/null \
+		 -sw "[%{http_code}] %{url}\n" '{}' \
+		| sort -u
+
 podchecker:
+	@echo "=======> Check PODs for syntax errors"
 	@podchecker *.pod
 
-.PHONY: podchecker
+.PHONY: urlcodes podchecker
