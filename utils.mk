1b67460099a248090133adb96fefa5150d87723b
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 07:17:48 +0300

    utils.mk: rework, add longlines target

--- utils.mk
+++ utils.mk
@@ -1,13 +1,22 @@
+GREPOPT = --exclude-dir=.git --exclude-dir=.github -R .
+FINDOPT = -not \( -path "./.git*" -or -path ".*~" \)
+MAXLINE = 80
+
 urlcodes:
 	@echo "=======> Check URLs for response code"
-	@grep -Eiho "https?://[^\"\\'> ]+" *.*      \
-		| grep -v fileserver.intranet       \
-		| xargs -P10 -I{} curl -o /dev/null \
-		 -sw "[%{http_code}] %{url}\n" '{}' \
+	@grep -Eiho "https?://[^\"\\'> ]+" ${GREPOPT} \
+		| grep -v fileserver.intranet         \
+		| xargs -P10 -I{} curl -o /dev/null   \
+		 -sw "[%{http_code}] %{url}\n" '{}'   \
 		| sort -u
 
 podchecker:
 	@echo "=======> Check PODs for syntax errors"
 	@podchecker *.pod
 
-.PHONY: urlcodes podchecker
+longlines:
+	@echo "=======> Check for long lines (> ${MAXLINE})"
+	@find . -type f ${FINDOPT} -exec awk -v ML=${MAXLINE} \
+		'length > ML { print FILENAME ":" FNR " " $$0 }' {} \;
+
+.PHONY: urlcodes podchecker longlines
