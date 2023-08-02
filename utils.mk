628598ae48c3483c7ec09dcf85fe36ff849770c0
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-02 04:48:04 +0300

    utils.mk: rework

--- utils.mk
+++ utils.mk
@@ -1,23 +1,18 @@
-GREPOPT = --exclude-dir=.git --exclude-dir=.github -R .
-FINDOPT = -not \( -path "./.git*" -or -path ".*~" \)
-MAXLINE = 80
-
 deadlinks:
-	@echo "=======> Check for dead links"
-	@grep -Eiho "https?://[^\"\\'> ]+" ${GREPOPT} \
-		| grep -v fileserver.intranet         \
-		| xargs -P10 -I{} curl -o /dev/null   \
-		 -sw "[%{http_code}] %{url}\n" '{}'   \
-		| grep -v '^\[200\]'                  \
+	@echo "=======> deadlinks"
+	@grep -EIihor "https?://[^\"\\'> ]+" --exclude-dir=.git*  \
+		| grep -v fileserver.intranet                     \
+		| xargs -P10 -r -I{} curl -I -o/dev/null          \
+		  -sw "[%{http_code}] %{url}\n" '{}'              \
+		| grep -v '^\[200\]'                              \
 		| sort -u
 
 podchecker:
-	@echo "=======> Check PODs for syntax errors"
+	@echo "=======> podchecker"
 	@podchecker *.pod
 
 longlines:
-	@echo "=======> Check for long lines (> ${MAXLINE})"
-	@find . -type f ${FINDOPT} -exec awk -v ML=${MAXLINE} \
-		'length > ML { print FILENAME ":" FNR " " $$0 }' {} \;
+	@echo "=======> longlines"
+	@grep -PIrn '^.{81,}$$' --exclude-dir=.git* || :
 
 .PHONY: deadlinks podchecker longlines
